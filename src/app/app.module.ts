8eb5e0c289f32a6157a165d702a8d10cb0fd5cc7
Shyou_92 <vitalex92@yandex.ru>
2022-05-03 17:41:02 +0300

    feat(components): add header and profile components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContainerComponent } from './container/container.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContainerComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
